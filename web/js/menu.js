1e7f8e1037f733d3a1e8d8e16425392df90468e9
kelly <kelly.cmsantos9@senacsp.edu.br>
2024-09-20 20:39:49 -0300

    reformulação

--- web/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("menu.html")
-        .then((response) => response.text())
-        .then((data) => {
-            document.getElementById("menu-container").innerHTML = data;
-        })
-        .catch((error) => console.error("Erro ao carregar o menu:", error));
-});
